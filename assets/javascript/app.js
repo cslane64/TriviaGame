342d336b33e2bebe8581027485107aab67661657
beeznme <cslane@gmail.com>
2019-04-01 15:34:05 -0400

    timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,47 @@
+$(document).ready(function(){
+
 var num = 30;
-var IntervalID;
+var intervalId;
 var correctGuesses;
 var wrongGuesses;
 var noGuess;
+var startButton = false; // check startbutton variable to determine whether or not to hide or show the button
+
+
+
+
+// Code for the timer
+
+$("#start-game").on("click", start);
+  
 
 function start() {
     clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
+    intervalId = setInterval(decreaseTime, 1000);
   }
 
 function decreaseTime(){
-    number--;
+    num--;
 
-    //  Show the number in the #show-number tag.
-    $("#show-number").html("<h2>" + number + "</h2>");
-
-
+    //  Show the number in the #timer div.
+    $("#timer").html("<h2> Time Remaining: 00:" + num + "</h2>");
     //  Once number hits zero...
-    if (number === 0) {
-
+    if (num === 0) {
+      console.log("done");
+      clearInterval(intervalId);
       //  ...pop up the next question.
       nextQuestion();
      //add 1 to the 'unguessed' variable
+
+    }
 }
 
 function nextQuestion() {
-
+    console.log("nextQuestion");
     //  Clears our intervalId
     //  We just pass the name of the interval
     //  to the clearInterval function.
-    clearInterval(intervalId);
+    
   }
-  start();
-}
+  //start();
+});
